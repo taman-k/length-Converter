64389e6f83ee40baa456043d3859bc12f9a39e97
Tamanna Khan <97503915+taman-k@users.noreply.github.com>
2023-03-06 21:26:57 +0530

    Add files via upload

---
+++ lengthconversion.py
@@ -0,0 +1,18 @@
+cm = int(input("enter length in centimeter:"))
+meter = cm/100.0
+kilometer = cm/1000.0
+inch = cm*0.3937
+print('length in meter :',meter)
+print('length in kilometer :',kilometer)
+print('length in inches :',inch)
+
+gram = int(input("enter weight in gram:"))
+kilogram = gram/1000
+pound = kilogram*2.2
+print("weight in kilogram:",kilogram)
+print("weight in pound:",pound)
+
+cels = int(input("enter temperature in celcius:"))
+fahr = cels * (9/5) +32
+print("temperature in fahrenheit:",fahr)
+
